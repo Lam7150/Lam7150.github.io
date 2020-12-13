4e073ef1e954ca5ba8d871d4480a6e08bffaaf36
Lam <Lam7150@gmail.com>
2020-12-12 19:33:49 -0500

    added button effects

--- src/App.js
+++ src/App.js
@@ -8,29 +8,29 @@ import pocket from "./assets/icons/pocket_white.png"
 
 function App() {
   return (
-    <div className="app-background h-screen w-screen flex items-center">
-      <div className="flex flex-col h-2/5 w-2/5 ml-48">
-        <div className="text-6xl text-white font-bold text-shadow-md">
+    <div className="app-background bg-center sm:bg-left-top h-screen w-screen flex justify-center items-center">
+      <div className="flex flex-col h-2/5 w-2/5 mr-120 mt-20">
+        <div className="text-3xl sm:text-5xl md:text-6xl text-white font-bold text-shadow-md">
           Hi, I'm Lam
         </div>
-        <div className="text-4xl text-white font-bold text-shadow-md">
+        <div className="text-2xl sm:text-3xl md:text-4xl text-white font-bold text-shadow-md">
           Welcome to my world.
         </div>
-        <div className="flex flex-row h-10 w-1/2 mt-3 justify-between">
-          <a href="mailto:lam7150@gmail.com">
-            <img src={telegram} className="h-full" alt="" />
+        <div className="flex flex-row h-12 mt-3">
+          <a href="mailto:lam7150@gmail.com" className="w-12 mr-4 button hvr-float">
+            <img src={telegram} alt="" />
           </a>
-          <a href="https://www.linkedin.com/in/lamgtran/">
-            <img src={linkedin} className="h-full" alt="" />
+          <a href="https://www.linkedin.com/in/lamgtran/" className="w-12 mr-4 button hvr-float">
+            <img src={linkedin} alt="" />
           </a>
-          <a href="https://www.github.com/lam7150">
-            <img src={github} className="h-full" alt="" />
+          <a href="https://www.github.com/lam7150" className="w-12 mr-4 button hvr-float">
+            <img src={github} alt="" />
           </a>
-          <a href="https://medium.com/@lam7150">
-            <img src={medium} className="h-full" alt="" />
+          <a href="https://medium.com/@lam7150" className="w-12 mr-4 button hvr-float">
+            <img src={medium} alt="" />
           </a>
-          <a href="https://drive.google.com/file/d/1OI7ARfocFdDIWRJ7lEKaNzWa88GPAdL4/view?usp=sharing">
-            <img src={pocket} className="h-full" alt="" />
+          <a href="https://drive.google.com/file/d/1OI7ARfocFdDIWRJ7lEKaNzWa88GPAdL4/view?usp=sharing" className="w-12 mr-4 button hvr-float">
+            <img src={pocket} alt="" />
           </a>
           <div />
         </div>
